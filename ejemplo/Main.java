6f5bf58a23b25cc40385656eec3cac6dfcebeb2e
carolinapena785 <carolinapena785@gmail.com>
2017-07-19 10:50:06 -0400

    funciom sumar

--- ejemplo/Main.java
+++ ejemplo/Main.java
@@ -4,5 +4,10 @@ package ejemplo;
     
       public static void main(String[] args){
         System.out.println("hola mundo");
+          int s= suma (20,30);
+          System.out.println(s);
       }
+    public static int suma(int numero1 , int numero2 ){
+      return numero1 + numero2;
+    }
   }
